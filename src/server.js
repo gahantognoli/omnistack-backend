92d1f2194b8429f51d7b288beb5ab1e0f651d44c
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2019-04-11 22:06:25 -0300

    Mudança packges

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 //qualquer front-end acessar a aplicação publicada. Caso isso não seja feito, 
 //a aplicação só poderá ser acessada por clients no mesmo domínio.
-app.use(cors() );
+app.use(cors());
 
 //cadastrar um módulo dentro do express
 app.use(express.json());
